86a9cfa8a1ab527e828871815a84352c73895a2a
sethan8r <sethan8r@ya.ru>
2025-12-18 13:42:11 +0300

    @Size and yandex metrika

--- src/main/java/dev/sethan8r/mama/dto/PicturesWorksRequestDto.java
+++ src/main/java/dev/sethan8r/mama/dto/PicturesWorksRequestDto.java
@@ -1,10 +1,13 @@
 package dev.sethan8r.mama.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record PicturesWorksRequestDto(
         @NotBlank(message = "Поле не должно быть пустым")
+        @Size(max = 100)
         String name,
+        @Size(max = 255)
         String description
 ) {
 }
--- src/main/java/dev/sethan8r/mama/dto/ProductRequestDto.java
+++ src/main/java/dev/sethan8r/mama/dto/ProductRequestDto.java
@@ -2,13 +2,16 @@ package dev.sethan8r.mama.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.util.Map;
 
 public record ProductRequestDto(
         @NotBlank(message = "Поле не должно быть пустым")
+        @Size(max = 100)
         String name,
         @NotBlank(message = "Поле не должно быть пустым")
+        @Size(max = 255)
         String description,
         String price,
         Map<String, String> characteristics,
